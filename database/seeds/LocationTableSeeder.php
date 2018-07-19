ac5c2019330f0445d2bf6bcd6b6bef1d3aa466cf
unknown <erfantkerfan@yahoo.com>
2018-07-19 14:35:58 +0430

    5shanbe shab

--- database/seeds/LocationTableSeeder.php
+++ database/seeds/LocationTableSeeder.php
@@ -7,9 +7,9 @@ class LocationTableSeeder extends Seeder
 
     public function run()
     {
-        $game_id = [1,3,5,7,9];
-        $location = ['','','','',''];
-        for ($i=0;$i<=4;$i++) {
+        $game_id = [1,3,5,9];
+        $location = ['','','',''];
+        for ($i=0;$i<=3;$i++) {
             \App\Location::create([
                 'game_id' => $game_id[$i],
                 'location' => $location[$i],
